7492fc24c79c8bcfe4cdba1a3320933ee64e1462
jeanetterosario88 <jeanetterosario88@gmail.com>
2020-09-29 20:47:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(namelist, event) {
+    let thankYouCards = []
+    for (let i = 0; i < namelist.length; i++) {
+        thankYouCards.push(`Thank you, ${namelist[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouCards
+}
+
+thenumber = 10
+function countDown(thenumber) {
+  while (thenumber > 0) {
+      console.log(thenumber); thenumber -= 1;
+  }
+console.log(thenumber)
+}
+
